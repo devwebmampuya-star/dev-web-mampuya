30a91ee38c257e428bc9d9881b895a57c6ef19d4
DEVESTEUR <devwebmampuya@gmail.com>
2026-06-11 03:21:14 +0200

    fix: ajout et configuration de l'integration Astro React pour Lucide Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react'; // 1. On importe le moteur React pour Astro
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  // Force Vercel à compiler le site même s'il y a des alertes de types mineures
+  // 2. On active React à côté de Tailwind dans les intégrations
+  integrations: [tailwind(), react()],
   vite: {
     logLevel: 'info',
   },
   typescript: {
-    // Désactive le blocage du build par TypeScript sur le serveur
     checkBenchmark: false,
     tsconfig: null
   }
